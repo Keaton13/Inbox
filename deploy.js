bf7b5a59501425cd894c2c7986dd95560d58a0a0
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-05-12 13:57:51 -0700

    Finished Test Contract

--- deploy.js
+++ deploy.js
@@ -1,10 +1,24 @@
 // deploy code will go here
-const HDWalletProvider = require('truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-    'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
+    'nephew river flight mesh any purpose gadget kind region brick west drift',
     'https://rinkeby.infura.io/v3/fd97f79d623645da92ce2305472cfcad'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+deploy();
